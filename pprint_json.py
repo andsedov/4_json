5d8e84f3b7801f44c9dde040043c82158d396d04
Sedov Andrey <sedov.andrey@bcg.com>
2018-06-25 15:46:14 +0300

    functions defined

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,21 @@ import json
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, encoding='utf-8') as file_object:
+        return json.load(file_object)
 
 
 def pretty_print_json(data):
-    pass
+    return json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        alco_shops = load_data('alco_shops.json')
+    except TypeError:
+        print('File path is not valid')
+
+    try:
+        print(json.dumps(alco_shops, sort_keys=True, indent=4, ensure_ascii=False))
+    except TypeError:
+        print('File is not valid')
